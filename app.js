84d1fbd846d27bfd4e5dcd76e8d495a50bd6506f
Andrés Felipe <aarcilab@gmail.com>
2025-01-23 22:45:29 -0500

    Agregando usuario a lista

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Crear variable array para almacenar la lista de amigos
+
+
+let amigos=[];
+
+function agregarAmigo(){ 
+    let obtenerAmigo= document.getElementById("amigo").value;
+
+    if(typeof obtenerAmigo == "string" && obtenerAmigo.length == 0){
+        alert("Por favor, inserte un nombre.");
+    }else{
+        amigos.push(obtenerAmigo);
+        console.log(amigos);
+    }
+    limpiarImput();
+}
+
+function limpiarImput() {
+
+document.getElementById("amigo").value = "";
+
+}
+
+function mostrarAmigos(){
+    document.getElementById("listaAmigos").value = "amigos";
+
+}
+
